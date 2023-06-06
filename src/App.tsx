4a3d93c1b6d738cc1c0599c8974df8a85eb84256
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-06-06 12:55:34 -0400

    handle hits

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import './App.css';
 import { useState } from 'react';
 
+interface Ship {
+  name: string;
+  coords: [number, number][];
+  hits: number;
+  sunk: boolean;
+}
+
 const initialBoard = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -12,14 +19,60 @@ const initialBoard = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+];
+
+const enemyShips: Ship[] = [
+  {
+    name: 'Carrier',
+    coords: [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]],
+    hits: 0,
+    sunk: false
+  },
+  {
+    name: 'Battleship',
+    coords: [[1, 0], [1, 1], [1, 2], [1, 3]],
+    hits: 0,
+    sunk: false
+  },
+  {
+    name: 'Cruiser',
+    coords: [[2, 0], [2, 1], [2, 2]],
+    hits: 0,
+    sunk: false
+  },
+  {
+    name: 'Submarine',
+    coords: [[3, 0], [3, 1], [3, 2]],
+    hits: 0,
+    sunk: false
+  },
+  {
+    name: 'Destroyer',
+    coords: [[4, 0], [4, 1]],
+    hits: 0,
+    sunk: false
+  }
+];
 
 function Board() {
   const [board, setBoard] = useState(initialBoard);
 
+  function checkForHit(rowIdx: number, colIdx: number) {
+    let hit = false;
+    enemyShips.forEach(ship => {
+      ship.coords.forEach(coord => {
+        if (coord[0] === rowIdx && coord[1] === colIdx) {
+          hit = true;
+        }
+      })
+    })
+    return hit;
+  }
+
   function handleCellClick(rowIdx: number, colIdx: number) {
     const newBoard = [...board];
-    newBoard[rowIdx][colIdx] = 1;
+    const hit = checkForHit(rowIdx, colIdx);
+    hit ? newBoard[rowIdx][colIdx] = 2 : newBoard[rowIdx][colIdx] = 1;
     setBoard(newBoard);
   }
 
@@ -31,7 +84,21 @@ function Board() {
             {
               row.map((cell, colIdx) => (
                 <div className="Cell" key={colIdx} onClick={() => handleCellClick(rowIdx, colIdx)}>
-                  { cell === 0 ? <div className="Empty"></div> : <div className="Filled"></div>}
+                  { 
+                    cell === 0 ? 
+                      <div className="Empty">
+                      </div> 
+                    : 
+                      <div className="Filled">
+                        {
+                          cell === 1 ?
+                            <div className="Miss">
+                            </div>
+                          :
+                            <div className="Hit">
+                            </div>
+                        }
+                      </div>}
                 </div>
               ))
             }
